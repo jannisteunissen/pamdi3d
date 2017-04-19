9c0416d389a453dcb0baa9be41c1e8996b9642b3
Casper Rutjes <casper@rutjes.pro>
2017-04-19 18:03:24 +0200

    Changed description of test

--- tests/test_m_photoi.f90
+++ tests/test_m_photoi.f90
@@ -1,6 +1,24 @@
+!  Casper Rutjes
+!
+! This program is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-!> electrons move in negative z direction
-!> we compute the positive velocity due to photo ionization
+!> Program to test the photo ionization module
+
+!> Set-up:
+!> Electrons move to negative z and are delected at z=0.
+!> Photo ionizations create new electrons at positive z locations
+!> which will move again to negative z and disappear at z=0.0.
 
 program test_m_photoi
   use m_pc_all
