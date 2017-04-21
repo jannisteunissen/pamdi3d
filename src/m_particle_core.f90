39dac2169b78b18019367b6e5d7ea7865852092e
Casper Rutjes <casper@rutjes.pro>
2017-04-21 18:57:23 +0200

    Bug fix add callback

--- src/m_particle_core.f90
+++ src/m_particle_core.f90
@@ -265,12 +265,17 @@ contains
     new_ionization_callback%ptr => new_ionization_callback_pptr
     if (allocated(self%ionization_callbacks)) then
       nn = size(self%ionization_callbacks)
-      if (nn>0) then
-      allocate(old_callback_list(nn))
-      deallocate(self%ionization_callbacks)
-      allocate(self%ionization_callbacks(nn+1))
-      self%ionization_callbacks(1:nn)=old_callback_list
-      self%ionization_callbacks(nn+1)=new_ionization_callback
+      if (nn.gt.0) then
+        allocate(old_callback_list(nn))
+        old_callback_list = self%ionization_callbacks
+        deallocate(self%ionization_callbacks)
+        allocate(self%ionization_callbacks(nn+1))
+        self%ionization_callbacks(1:nn)=old_callback_list
+        self%ionization_callbacks(nn+1)=new_ionization_callback
+      elseif (nn.eq.0) then
+        deallocate(self%ionization_callbacks)
+        allocate(self%ionization_callbacks(1))
+        self%ionization_callbacks = new_ionization_callback
       end if
     else
       allocate(self%ionization_callbacks(1))
@@ -287,20 +292,25 @@ contains
     type(callback_t), allocatable :: old_callback_list(:)
     integer                       :: nn
     
-    new_attachment_callback%ptr=>new_attachment_callback_pptr
+    new_attachment_callback%ptr => new_attachment_callback_pptr
     if (allocated(self%attachment_callbacks)) then
       nn = size(self%attachment_callbacks)
-      if (nn>0) then
-      allocate(old_callback_list(nn))
-      deallocate(self%attachment_callbacks)
-      allocate(self%attachment_callbacks(nn+1))
-      self%attachment_callbacks(1:nn)=old_callback_list
-      self%attachment_callbacks(nn+1)=new_attachment_callback
+      if (nn.gt.0) then
+        allocate(old_callback_list(nn))
+        old_callback_list = self%attachment_callbacks
+        deallocate(self%attachment_callbacks)
+        allocate(self%attachment_callbacks(nn+1))
+        self%attachment_callbacks(1:nn)=old_callback_list
+        self%attachment_callbacks(nn+1)=new_attachment_callback
+      elseif (nn.eq.0) then
+        deallocate(self%attachment_callbacks)
+        allocate(self%attachment_callbacks(1))
+        self%attachment_callbacks = new_attachment_callback
       end if
     else
       allocate(self%attachment_callbacks(1))
       self%attachment_callbacks = new_attachment_callback
-    end if    
+    end if
   end subroutine add_attachment_callback
 
   !> Initialization routine for the particle module
